4fb33860a23dc08dfa296dd4980874315b9569f1
sandje00 <stella.andjelic.00@fesb.hr>
2020-05-31 19:46:20 +0200

    Filter to constant memory

--- nra/kernel.cu
+++ nra/kernel.cu
@@ -11,10 +11,31 @@
 using namespace std;
 using namespace cv;
 
-void convolution(float3*, float*, int, int, float3*);
+
+// Laplacian
+/*__constant__ float filter[ROWS * COLUMNS] = {
+						-1, -1, -1, -1, -1,
+						-1, -1, -1, -1, -1,
+						-1, -1, 24, -1, -1,
+						-1, -1, -1, -1, -1,
+						-1, -1, -1, -1, -1
+					};*/
+
+// Laplacian of Gaussian
+__constant__ float filter[ROWS * COLUMNS] = {
+						0, 0, -1, 0, 0,
+						0, -1, -2, -1, 0,
+						-1, -2, 16, -2, -1,
+						0, -1, -2, -1, 0,
+						0, 0, -1, 0, 0
+					};
+
+
+void convolution(float3*, int, int, float3*);
 __device__ float3 add(float3, float3);
 __device__ float3 multiply(float, float3);
-__global__ void kernel(float3*, float*, int, int, float3*);
+__global__ void kernel(float3*, int, int, float3*);
+
 
 int main() {
 	Mat Image = imread("flower.png", IMREAD_COLOR);
@@ -32,26 +53,8 @@ int main() {
 	Mat Result(height, width, Image.type());
 	float3* input = (float3*)Image.ptr<float3>();
 	float3* output = (float3*)Result.ptr<float3>();
-
-	// Laplacian
-	/*float filter[ROWS * COLUMNS] = {
-		-1, -1, -1, -1, -1,
-		-1, -1, -1, -1, -1,
-		-1, -1, 24, -1, -1,
-		-1, -1, -1, -1, -1,
-		-1, -1, -1, -1, -1
-	};*/
 	
-	// Laplacian of Gaussian
-	float filter[ROWS * COLUMNS] = {
-		0, 0, -1, 0, 0,
-		0, -1, -2, -1, 0,
-		-1, -2, 16, -2, -1,
-		0, -1, -2, -1, 0,
-		0, 0, -1, 0, 0
-	};
-	
-	convolution(input, filter, height, width, output);
+	convolution(input, height, width, output);
 
 	Result *= 255;
 	Result.convertTo(Result, CV_8UC3);
@@ -63,56 +66,67 @@ int main() {
 	return 0;
 }
 
-void convolution(float3* input, float* filter, int H, int W, float3* output) {
+
+void convolution(float3* input, int H, int W, float3* output) {
 	float3* dev_input = NULL;
 	float3* dev_output = NULL;
-	float* dev_filter = NULL;
+
 	int size = H * W * sizeof(float3);
+
 	cudaError_t cudaStatus;
+	cudaEvent_t start, stop;
+
+	cudaEventCreate(&start);
+	cudaEventCreate(&stop);
 
 	cudaStatus = cudaMalloc((void**)&dev_input, size*sizeof(float3));
 	if (cudaStatus != cudaSuccess)
-		fprintf(stderr, "cudaMalloc dev_input failed!");
+		fprintf(stderr, "cudaMalloc dev_input failed!\n");
 
 	cudaStatus = cudaMalloc((void**)&dev_output, size * sizeof(float3));
 	if (cudaStatus != cudaSuccess)
-		fprintf(stderr, "cudaMalloc dev_output failed!");
-
-	cudaStatus = cudaMalloc((void**)&dev_filter, ROWS * COLUMNS * sizeof(float));
-	if (cudaStatus != cudaSuccess)
-		fprintf(stderr, "cudaMalloc dev_filter failed!");
+		fprintf(stderr, "cudaMalloc dev_output failed!\n");
 
 	cudaStatus = cudaMemcpy(dev_input, input, size, cudaMemcpyHostToDevice);
 	if (cudaStatus != cudaSuccess)
-		fprintf(stderr, "cudaMemcpyHostToDevice dev_input failed!");
-
-	cudaStatus = cudaMemcpy(dev_filter, filter, ROWS * COLUMNS * sizeof(float), cudaMemcpyHostToDevice);
-	if (cudaStatus != cudaSuccess)
-		fprintf(stderr, "cudaMemcpyHostToDevice dev_filter failed!");
+		fprintf(stderr, "cudaMemcpyHostToDevice dev_input failed!\n");
 
 	dim3 dimGrid(ceil((float)W / 16), ceil((float)H / 16));
 	dim3 dimBlock(16, 16, 1);
 
-	kernel<<<dimGrid, dimBlock>>>(dev_input, dev_filter, H, W, dev_output);
+	cudaEventRecord(start, 0);
+	kernel<<<dimGrid, dimBlock>>>(dev_input, H, W, dev_output);
+	cudaEventRecord(stop, 0);
 
 	cudaStatus = cudaMemcpy(output, dev_output, size, cudaMemcpyDeviceToHost);
 	if (cudaStatus != cudaSuccess)
-		fprintf(stderr, "cudaMemcpyDeviceToHost img failed!");
+		fprintf(stderr, "cudaMemcpyDeviceToHost img failed!\n");
+
+	cudaEventSynchronize(stop);
+	float milliseconds = 0;
+	cudaEventElapsedTime(&milliseconds, start, stop);
+
+	cout << "It took " << milliseconds << " milliseconds" << endl;
+
+	cudaEventDestroy(start);
+	cudaEventDestroy(stop);
 
 	cudaFree(dev_input);
 	cudaFree(dev_output);
-	cudaFree(dev_filter);
 }
+
 
 __device__ float3 add(float3 vec1, float3 vec2) {
 	return{ vec1.x + vec2.x, vec1.y + vec2.y, vec1.z + vec2.z };
 }
 
+
 __device__ float3 multiply(float num, float3 vec) {
 	return{ num * vec.x, num * vec.y, num * vec.z };
 }
 
-__global__ void kernel(float3* dev_input, float* filter, int H, int W, float3* dev_output) {
+
+__global__ void kernel(float3* dev_input, int H, int W, float3* dev_output) {
 	int col = threadIdx.x + blockIdx.x * blockDim.x;
 	int row = threadIdx.y + blockIdx.y * blockDim.y;
 	int rowsRadius = ROWS / 2;
